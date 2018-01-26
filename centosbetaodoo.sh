8c931610d1603163c377beebc8d69d685d8acbf0
shinjievil <shinjievil@gmail.com>
2018-01-26 09:24:04 -0500

    Update centosbetaodoo.sh

--- centosbetaodoo.sh
+++ centosbetaodoo.sh
@@ -52,7 +52,9 @@ sudo yum upgrade -y
 # Install PostgreSQL Server
 #--------------------------------------------------
 echo -e "\n---- Install PostgreSQL Server ----"
-sudo yum install postgresql -y
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install postgresql-9.6
 
 echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
 sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
